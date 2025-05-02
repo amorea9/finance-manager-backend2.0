5d2808e2ef9a13081a8d9c8ac6c688b49f871a6d
Alessia <98785381+amorea9@users.noreply.github.com>
2025-05-02 12:07:43 +0200

    mutation changes

--- src/migrations/1740398796820-addedDescription.ts
+++ src/migrations/1740398796820-addedDescription.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddedDescription1740398796820 implements MigrationInterface {
-    name = 'AddedDescription1740398796820'
+  name = 'AddedDescription1740398796820';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "category" ADD "description" character varying NOT NULL DEFAULT ''`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "category" ADD "description" character varying NOT NULL DEFAULT ''`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "category" DROP COLUMN "description"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "category" DROP COLUMN "description"`);
+  }
 }
